f5dbdb51c70fa0c212300d21da39b92d210db85f
Henrique Matheus Alves Pereira <henrique.map@outlook.com>
2021-01-30 11:53:05 -0300

    fix: lblDesc now is display the info
    The problem was the fixed-width constrain of the lblNum. When I changed it to a width less than or equal to the distance from lblDesc, the table on the device simulator start working showing the fruit's from the array.

--- TableViewExample/TableViewExample/ViewController.swift
+++ TableViewExample/TableViewExample/ViewController.swift
@@ -7,24 +7,36 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {    
 
+    var frutas = ["Abacate", "Abacaxi", "Acerola", "Ameixa", "Banana", "Caju", "Carambola", "Damasco", "Figo", "Goiaba", "Graviola", "Jaca", "Laranja", "Limão", "Maçã", "Manga", "Maracujá", "Melancia", "Melão", "Nectarina", "Pêssego", "Pitanga", "Romã", "Tangerina", "Uva"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        self.title = "Frutas"
     }
     
-    func tableView
-
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! CustomTableViewCell
-        cell.lblDesc.text = "Texto da linha"
-        cell.lblNum.text = String(indexPath.row + 1)
-        return cell
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.(withIdentifier: "Cell", for: indexPath)
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return frutas.count
     }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! CustomTableViewCell
+        
+        cell.lblDesc.text = frutas[indexPath.row]
+        
+        cell.lblNum.text = String(indexPath.row + 1)
+        
+        return cell
+    }
+    
 }
 
