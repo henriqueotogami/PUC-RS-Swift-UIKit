90148ca234eef58b0e1647d3df29bb5dfd3c2a4b
Henrique Matheus Alves Pereira <henrique.map@outlook.com>
2021-01-30 17:13:22 -0300

    fix: finished fourth class

--- TableViewExample/TableViewExample/ViewController.swift
+++ TableViewExample/TableViewExample/ViewController.swift
@@ -14,8 +14,6 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        
-        self.title = "Frutas"
     }
     
     override func didReceiveMemoryWarning() {
@@ -37,6 +35,4 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         
         return cell
     }
-    
 }
-
