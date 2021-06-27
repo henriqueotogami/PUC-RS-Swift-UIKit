c94b37938fcf1c34299aaa01ea939316815d4d1a
Henrique Matheus Alves Pereira <henrique.map@outlook.com>
2021-06-27 16:20:54 -0300

    Cadastro e login com Firebase concluído

--- FirstApp/FirstApp/ViewController.swift
+++ FirstApp/FirstApp/ViewController.swift
@@ -8,106 +8,98 @@
 import UIKit
 import Firebase
 
-
-
-
-class ViewController: UIViewController {
+class LoginViewController: UIViewController {
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    var auth:Auth?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.auth = Auth.auth()
     }
     
+    func alert(title: String, message: String) {
+        
+        let alertController: UIAlertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let ok: UIAlertAction = UIAlertAction(title: "ok", style: .cancel, handler: nil)
+        alertController.addAction(ok)
+        self.present(alertController, animated: true, completion: nil)
+    }
 
+    @IBAction func tappedLoginButton(_ sender: UIButton) {
+        
+        let email:String = self.emailTextField.text ?? ""
+        let senha:String = self.passwordTextField.text ?? ""
+        
+        self.auth?.signIn(withEmail: email, password: senha, completion: { (usuario, error) in
+            
+            if error != nil {
+                self.alert(title: "Atenção", message: "Dados incorretos, tente novamente.")
+                //print("Dados incorretos, tente novamente.")
+            }else{
+                if usuario == nil {
+                    self.alert(title: "Atenção", message: "Tivemos um problema inesperado.")
+                    //print("Tivemos um problema inesperado.")
+                }else{
+                    self.alert(title: "Atenção", message: "Login realizado com sucesso.")
+                    //print("Login realizado com sucesso.")
+                }
+            }
+        })
+    }
     
-    class LoginViewController: UIViewController {
-        
-        @IBOutlet weak var emailTextField: UITextField!
-        @IBOutlet weak var passwordTextField: UITextField!
-        
-        var auth:Auth?
-        
-        override func viewDidLoad() {
-            super.viewDidLoad()
-            // Do any additional setup after loading the view.
-            self.auth = Auth.auth()
-        }
-        
-        func alert(title: String, message: String) {
-            
-            let alertController: UIAlertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
-            let ok: UIAlertAction = UIAlertAction(title: "ok", style: .cancel, handler: nil)
-            alertController.addAction(ok)
-            self.present(alertController, animated: true, completion: nil)
-        }
-        
-        @IBAction func tappedLoginButton(_ sender: UIButton) {
-            
-            let email:String = self.emailTextField.text ?? ""
-            let senha:String = self.passwordTextField.text ?? ""
-            
-            self.auth?.signIn(withEmail: email, password: senha, completion: { (usuario, error) in
-                
-                if error != nil {
-                    self.alert(title: "Atenção", message: "Dados incorretos, tente novamente.")
-                    //print("Dados incorretos, tente novamente.")
-                }else{
-                    if usuario == nil {
-                        self.alert(title: "Atenção", message: "Tivemos um problema inesperado.")
-                        //print("Tivemos um problema inesperado.")
-                    }else{
-                        self.alert(title: "Atenção", message: "Login realizado com sucesso.")
-                        //print("Login realizado com sucesso.")
-                    }
-                }
-            })
-        }
-        
-        @IBAction func tappedRegisterButton(_ sender: UIButton){
-            self.performSegue(withIdentifier: "RegisterViewController", sender: nil)
-        }
-        
+    @IBAction func tappedRegisterButton(_ sender: UIButton){
+        self.performSegue(withIdentifier: "RegisterViewController", sender: nil)
     }
-
-    class RegisterViewController: UIViewController {
-        
-        @IBOutlet weak var emailTextField: UITextField!
-        @IBOutlet weak var passwordTextField: UITextField!
-        
-        var auth:Auth?
-        
-        override func viewDidLoad() {
-            super.viewDidLoad()
-            // Do any additional setup after loading the view.
-            self.auth = Auth.auth()
-        }
-        
-        func alert(title: String, message: String) {
-            
-            let alertController: UIAlertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
-            let ok: UIAlertAction = UIAlertAction(title: "ok", style: .cancel, handler: nil)
-            alertController.addAction(ok)
-            self.present(alertController, animated: true, completion: nil)
-        }
-        
-        @IBAction func tappedRegisterButton(_ sender: UIButton) {
-            
-            let email:String = self.emailTextField.text ?? ""
-            let senha:String = self.passwordTextField.text ?? ""
-            
-            self.auth?.createUser(withEmail: email, password: senha, completion: { (result, error) in
-                
-                if error != nil {
-                    self.alert(title: "Atenção", message: "Falha ao cadastrar.")
-                    // print("Falha ao cadastrar.")
-                }else{
-                    self.alert(title: "Parabéns", message: "Sucesso ao cadastrar.")
-                    // print("Sucesso ao cadastrar")
-                }
-            })
-        }
-    }
-
+    
 }
 
+class RegisterViewController: UIViewController {
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    var auth:Auth?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        self.auth = Auth.auth()
+    }
+    
+    func alert(title: String, message: String) {
+        
+        let alertController: UIAlertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let ok: UIAlertAction = UIAlertAction(title: "ok", style: .cancel, handler: nil)
+        alertController.addAction(ok)
+        self.present(alertController, animated: true, completion: nil)
+    }
+    
+    @IBAction func tappedRegisterButton(_ sender: UIButton) {
+        
+        let email:String = self.emailTextField.text ?? ""
+        let senha:String = self.passwordTextField.text ?? ""
+        
+        self.auth?.createUser(withEmail: email, password: senha, completion: { (result, error) in
+            
+            if error != nil {
+                self.alert(title: "Atenção", message: "Falha ao cadastrar.")
+                // print("Falha ao cadastrar.")
+            }else{
+                self.alert(title: "Parabéns", message: "Sucesso ao cadastrar.")
+                // print("Sucesso ao cadastrar")
+            }
+        })
+    }
+}
 
+class InfoPersonProfile: UIViewController {
+    
+    @IBOutlet weak var personName: UITextField!
+    @IBOutlet weak var personEmail: UITextField!
+    
+
+}
